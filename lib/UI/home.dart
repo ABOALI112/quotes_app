f44612c54cc0e380137249f54fdf50f6bb590a92
ABOALI112 <social.101.acc@gmail.com>
2022-02-20 13:36:09 +0200

    adding a list of quotes and styling the buttons and text

--- lib/UI/home.dart
+++ lib/UI/home.dart
@@ -8,8 +8,32 @@ class Wisdom extends StatefulWidget {
 }
 
 class _WisdomState extends State<Wisdom> {
+  final List Quotes = [
+    "”It is hard to fail but it is worse never to have tried to succeed”.—Theodore Roosevelt",
+    "“It is hard to fail but it is worse never to have tried to succeed.”—Friedrich Nietzsche",
+    "“I have not failed. I’ve just found 10,000 ways that won’t work.” —Thomas A. Edison",
+    "“Be miserable. Or motivate yourself. Whatever has to be done, it’s always your choice.” —Wayne Dyer",
+    "“The future belongs to those who believe in the beauty of their dreams.” —Eleanor Roosevelt",
+    "“I challenge you to make your life a masterpiece. I challenge you to join the ranks of those"
+        " people who live what they teach, who walk their talk.” —Anthony Robbins",
+    "“The greater damage for most of us is not that our aim is too high and we miss it, but that it it too low and we reach it.” —Michelangelo"
+        "“Do not go where the path may lead , go instead where there is no path and leave a trail.” —Ralph Waldo Emerson",
+    "Success is not final, failure is not fatal: it is the courage to continue that counts. —Winston Churchill"
+        "“I hated every minute of training, but I said, ‘Don’t quit. Suffer now and live the rest of your life as a champion.’” —Muhammad Ali",
+    "“I’ve missed more than 9000 shots in my career. I’ve lost almost 300 games. 26 times I’ve been trusted to"
+        " take the game-winning shot and missed. I’ve failed over and over and over again in my life. And that is why I succeed.” —Michael Jordan",
+    "“The two most important days in your life are the day you are born and the day you find out why.” —Mark Twain",
+    "“When everything seems to be going against you, remember that the airplane takes off against the wind, not with it.” —Henry Ford",
+    "“Live each day as if your life had just begun.” —Johann Wolfgang Von Goethe",
+    "“The journey of a thousand miles begins with one step.” —Lao Tzu"
+  ];
   @override
   Widget build(BuildContext context) {
+    TextStyle styles = const TextStyle(
+      fontWeight: FontWeight.bold,
+      fontSize: 18,
+      color: Colors.black54,
+    );
     return MaterialApp(
       title: "Quotes App",
       home: Scaffold(
@@ -17,15 +41,27 @@ class _WisdomState extends State<Wisdom> {
           child: Column(
             mainAxisAlignment: MainAxisAlignment.center,
             children: [
-              const Text("First Quote"),
+              Text(
+                Quotes[0],
+                style: styles,
+              ),
               TextButton.icon(
-                  onPressed: null,
-                  icon: const Icon(Icons.wb_sunny),
-                  label: const Text("Inspire Me"))
+                onPressed: _showQuote,
+                icon: const Icon(
+                  Icons.wb_sunny,
+                  color: Colors.black,
+                ),
+                label: Text(
+                  "Inspire Me",
+                  style: styles.copyWith(fontSize: 16.0),
+                ),
+              )
             ],
           ),
         ),
       ),
     );
   }
+
+  void _showQuote() {}
 }
